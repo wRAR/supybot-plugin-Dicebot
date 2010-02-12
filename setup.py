b0d7cca7516169acd554056ce2b15b28ae428bb3
Andrey Rahmatullin <wrar@altlinux.ru>
2010-02-13 00:33:15 +0500

    bump version to 0.5
    Signed-off-by: Andrey Rahmatullin <wrar@altlinux.ru>

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ package_data = { }
 for plugin in plugins:
     package_dir['supybot.plugins.' + plugin] = plugin
 
-version = '0.4'
+version = '0.5'
 setup(
     name='supybot-plugin-dicebot',
     version=version,
